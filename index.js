820a2651781bdb001aeef11fca13e060834bd984
Samarth <samarth.srikar@students.iiit.ac.in>
2024-04-11 17:19:08 +0530

    updated insta posting

--- index.js
+++ index.js
@@ -42,13 +42,26 @@ function get_info()
             is_valid_return=-1; // Return -1 if the error indicates document not found
             console.log(is_valid_return)
         }
-        else if(data.platform == 1 || data.platform == 3)
-        {
-        imageUrl = data.image_url
-        description = data.description
-        width = data.width
-        height = data.height;
-        postToInsta();
+        else if (data.platform == 1 || data.platform == 4) {
+            imageUrl = data.image_url
+            description = data.description
+            width = data.width
+            height = data.height;
+            postToInsta();
+        }
+        else if (data.platform == 3) {
+            imageUrl = data.image_url
+            description = data.description
+            width = data.width
+            height = data.height;
+            addStoryToInsta();
+        }
+        else if (data.platform == 5) {
+            imageUrl = data.image_url
+            description = data.description
+            width = data.width
+            height = data.height;
+            post_and_add_story();
         }
     })
     .catch(error => {
@@ -58,54 +71,185 @@ function get_info()
 }
 
 const postToInsta = async () => {
+    try {
+        const ig = new IgApiClient();
+        ig.state.generateDevice(process.env.IG_USERNAME);
+        await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+        console.log('Login Successful.');
 
-    const ig = new IgApiClient();
-    ig.state.generateDevice(process.env.IG_USERNAME);
-    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-  
-    const image = await Jimp.read({
-        url: imageUrl
-    });
-    console.log('Login Successful.');
-    
-    await image.resize(width,height, Jimp.RESIZE_NEAREST_NEIGHBOR)
-           .quality(100)
-  
-    const resizedImageBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-    const imageBuffer = resizedImageBuffer
-    await ig.publish.photo({
-        file: imageBuffer,
-        caption: description,
-    });
-    console.log('Posted successfully.');    
-}
+        const image = await Jimp.read({ url: imageUrl });        
+
+        const overlay = await Jimp.read({url:'https://github.com/Samarth23-sudo/ClubArtizen/blob/main/Club%20Artizen%20Logo%20Circle.png?raw=true'});
+        
+        if (!image || !overlay) {
+            throw new Error('Failed to load images.');
+        }
+        
+        await image.resize(width, height, Jimp.RESIZE_NEAREST_NEIGHBOR).quality(100);
+
+        overlay.resize(width/6.5, width/6.5);
+
+        image.composite(overlay, 0, 0, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacityDest: 1,
+            opacitySource: 1
+        });
+
+        await image.writeAsync("result.jpg");
+
+        const resizedImageBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+        const imageBuffer = resizedImageBuffer;
+
+        await ig.publish.photo({
+            file: imageBuffer,
+            caption: description,
+        });
+
+        console.log('Posted successfully.');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
 
 const addStoryToInsta = async () => {
-    try{
+    try {
+        const ig = new IgApiClient();
+        ig.state.generateDevice(process.env.IG_USERNAME);
+        await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+        console.log('Login Successful.');
+
+        const image = await Jimp.read({ url: imageUrl });
+
+        const overlay = await Jimp.read({url:'https://github.com/Samarth23-sudo/ClubArtizen/blob/main/Club%20Artizen%20Logo%20Circle.png?raw=true'});
         
-    const ig = new IgApiClient();
-    ig.state.generateDevice(process.env.IG_USERNAME);
-    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+        if (!image || !overlay) {
+            throw new Error('Failed to load images.');
+        }
+        
+        overlay.resize(width/5, width/5);
 
-    const image = await Jimp.read({
-        url: imageUrl
-    });
-    console.log('Login Successful.');
-    
-    await image.resize(width,height, Jimp.RESIZE_NEAREST_NEIGHBOR)
-           .quality(100)
-    
-    const resizedImageBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-    const imageBuffer = resizedImageBuffer
+        image.composite(overlay, 0, 0, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacityDest: 1,
+            opacitySource: 1
+        });
+        // Resize the image to fit within the 9:16 aspect ratio
+        const aspectRatio = width / height;
+        const desiredWidth = 1080;
+        const desiredHeight = 1920;
 
-    await ig.publish.story({
-        file: imageBuffer,
-        caption: "ClubArtizen",        
-    });
+        let newWidth, newHeight;
+        if (aspectRatio > desiredWidth / desiredHeight) {
+            // Fit to width
+            newWidth = desiredWidth;
+            newHeight = Math.round(newWidth / aspectRatio);
+        } else {
+            // Fit to height
+            newHeight = desiredHeight;
+            newWidth = Math.round(newHeight * aspectRatio);
+        }
 
-    console.log('story uploaded successfully.');
-    }catch (error) {
+        // Resize the image
+        await image.resize(newWidth, newHeight);
+        
+        
+        // Create a new image with the desired aspect ratio and fill it with white color
+        const paddedImage = new Jimp(desiredWidth, desiredHeight, 0xFFFFFFFF);
+
+        // Paste the resized image onto the padded image to center it
+        const xOffset = Math.floor((desiredWidth - newWidth) / 2);
+        const yOffset = Math.floor((desiredHeight - newHeight) / 2);
+        paddedImage.composite(image, xOffset, yOffset);
+
+        // Convert the image to buffer
+        const imageBuffer = await paddedImage.getBufferAsync(Jimp.MIME_JPEG);
+
+        // Publish the story with the padded image
+        await ig.publish.story({
+            file: imageBuffer,
+            caption: "ClubArtizen",
+        });
+
+        console.log('Story Posted successfully.');
+    } catch (error) {
         console.error('Error occurred while publishing the story:', error);
+    }
+};
+
+const post_and_add_story = async () => {
+    try {
+        const ig = new IgApiClient();
+        ig.state.generateDevice(process.env.IG_USERNAME);
+        await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+
+        const image = await Jimp.read({ url: imageUrl });
+        console.log('Login Successful.');
+
+        await image.resize(width, height, Jimp.RESIZE_NEAREST_NEIGHBOR).quality(100);
+
+        const overlay = await Jimp.read({url:'https://github.com/Samarth23-sudo/ClubArtizen/blob/main/Club%20Artizen%20Logo%20Circle.png?raw=true'});
+        
+        if (!image || !overlay) {
+            throw new Error('Failed to load images.');
+        }
+        
+        overlay.resize(width/6.5, width/6.5);
+
+        image.composite(overlay, 0, 0, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacityDest: 1,
+            opacitySource: 1
+        });
+
+        const resizedImageBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+        const imageBuffer = resizedImageBuffer;
+
+        await ig.publish.photo({
+            file: imageBuffer,
+            caption: description,
+        });
+        console.log('Posted successfully.');
+
+        //Fix the size of the image on story
+        // Resize the image to fit within the 9:16 aspect ratio
+        const aspectRatio = width / height;
+        const desiredWidth = 1080;
+        const desiredHeight = 1920;
+
+        let newWidth, newHeight;
+        if (aspectRatio > desiredWidth / desiredHeight) {
+            // Fit to width
+            newWidth = desiredWidth;
+            newHeight = Math.round(newWidth / aspectRatio);
+        } else {
+            // Fit to height
+            newHeight = desiredHeight;
+            newWidth = Math.round(newHeight * aspectRatio);
+        }
+
+        // Resize the image
+        await image.resize(newWidth, newHeight);
+
+        // Create a new image with the desired aspect ratio and fill it with white color
+        const paddedImage = new Jimp(desiredWidth, desiredHeight, 0xFFFFFFFF);
+
+        // Paste the resized image onto the padded image to center it
+        const xOffset = Math.floor((desiredWidth - newWidth) / 2);
+        const yOffset = Math.floor((desiredHeight - newHeight) / 2);
+        paddedImage.composite(image, xOffset, yOffset);
+
+        // Convert the image to buffer
+        const imageBuffer_1 = await paddedImage.getBufferAsync(Jimp.MIME_JPEG);
+
+        // Publish the story with the padded image
+        await ig.publish.story({
+            file: imageBuffer_1,
+            caption: "ClubArtizen",
+        });
+
+        console.log('Story Posted successfully.');
+    } catch (error) {
+        console.error('Error:', error.message);
     }
 };
 
